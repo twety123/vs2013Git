a2e2e72f4c3eb127591316e1addb44405df346e8
twety123@qq.com <twety123@qq.com>
2014-10-14 21:43:06 +0800

    first test commit

--- firstMVC/Models/AccountModels.cs
+++ firstMVC/Models/AccountModels.cs
@@ -30,7 +30,7 @@ namespace firstMVC.Models
     public class RegisterExternalLoginModel
     {
         [Required]
-        [Display(Name = "用户名")]
+        [Display(Name = "用户名aa")]
         public string UserName { get; set; }
 
         public string ExternalLoginData { get; set; }
